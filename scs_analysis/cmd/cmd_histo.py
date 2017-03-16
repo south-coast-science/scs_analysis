11c40a433508030c173612bfe184196539fa3a8c
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-03-16 18:46:35 +0000

    Cleaning.

--- scs_analysis/cmd/cmd_histo.py
+++ scs_analysis/cmd/cmd_histo.py
@@ -18,8 +18,8 @@ class CmdHisto(object):
         """
         Constructor
         """
-        self.__parser = optparse.OptionParser(usage="%prog PATH [-b] [-x MIN MAX] [-c BINCOUNT] [-o FILENAME] [-e] [-v]",
-                                              version="%prog 1.0")
+        self.__parser = optparse.OptionParser(usage="%prog PATH [-b] [-x MIN MAX] [-c BINCOUNT] [-o FILENAME] [-e] "
+                                                    "[-v]", version="%prog 1.0")
 
         # optional...
         self.__parser.add_option("--batch", "-b", action="store_true", dest="batch_mode", default=False,
