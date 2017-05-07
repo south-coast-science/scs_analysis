18a28ae2cc544e1efc6f2d9267f365424c121422
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-05-07 14:55:05 +0100

    Upgraded control channel scripts.

--- scs_analysis/control_publication_runner.sh
+++ scs_analysis/control_publication_runner.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# use this script to keep the control_publication_pipe open
+# use this script to keep the control_publication_pipe open with nohup control_publication_runner.sh &
 
-while true; do sleep 1; done > ~/SCS/pipes/control_publication_pipe
+while true; do sleep 60; done > ~/SCS/pipes/control_publication_pipe
