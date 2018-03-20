1f23df055c266a81b3ded0884b484a1dfd83cadf
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-03-20 17:25:17 +0000

    Added MedianFilter.

--- src/scs_analysis/cmd/cmd_sample_median.py
+++ src/scs_analysis/cmd/cmd_sample_median.py
@@ -9,7 +9,7 @@ import optparse
 
 # --------------------------------------------------------------------------------------------------------------------
 
-class CmdLowPassFilter(object):
+class CmdSampleMedian(object):
     """
     unix command line handler
     """
@@ -18,17 +18,13 @@ class CmdLowPassFilter(object):
         """
         Constructor
         """
-        self.__parser = optparse.OptionParser(usage="%prog -d DELTA_T -c CUT_OFF [-p PRECISION] [-v] PATH",
+        self.__parser = optparse.OptionParser(usage="%prog [-w SIZE] [-p PRECISION] [-v] PATH",
                                               version="%prog 1.0")
 
-        # compulsory...
-        self.__parser.add_option("--delta", "-d", type="float", nargs=1, action="store", dest="delta",
-                                 help="sampling time interval")
-
-        self.__parser.add_option("--cut-off", "-c", type="float", nargs=1, action="store", dest="cut_off",
-                                 help="cut-off frequency")
-
         # optional...
+        self.__parser.add_option("--window", "-w", type="int", nargs=1, action="store", dest="window", default=3,
+                                 help="window size (must be an odd number, default 3)")
+
         self.__parser.add_option("--prec", "-p", type="int", nargs=1, action="store", default=None, dest="precision",
                                  help="precision (default 0 decimal places)")
 
@@ -41,7 +37,7 @@ class CmdLowPassFilter(object):
     # ----------------------------------------------------------------------------------------------------------------
 
     def is_valid(self):
-        if self.delta is None or self.cut_off is None:
+        if self.window is not None and self.window % 2 == 0:
             return False
 
         return True
@@ -55,13 +51,8 @@ class CmdLowPassFilter(object):
 
 
     @property
-    def delta(self):
-        return self.__opts.delta
-
-
-    @property
-    def cut_off(self):
-        return self.__opts.cut_off
+    def window(self):
+        return self.__opts.window
 
 
     @property
@@ -86,5 +77,5 @@ class CmdLowPassFilter(object):
 
 
     def __str__(self, *args, **kwargs):
-        return "CmdLowPassFilter:{path:%s, delta:%s, cut_off:%s, verbose:%s, precision:%s, args:%s}" % \
-               (self.path, self.delta, self.cut_off, self.verbose, self.precision, self.args)
+        return "CmdSampleMedian:{path:%s, window:%s, verbose:%s, precision:%s, args:%s}" % \
+               (self.path, self.window, self.verbose, self.precision, self.args)
