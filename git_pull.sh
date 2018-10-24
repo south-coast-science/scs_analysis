13f5e5b5b4f54cbbafd87b64cc329be2b40ae332
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-10-24 12:44:37 +0100

    Updated git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# WARNING: if your installation includes scs_dev, use that git_pull script, and not this one.
+# WARNING: if your installation includes scs_dev, use the scs_dev git_pull script, and not this one.
 
 GIT_PATH=~/SCS/scs_analysis/
 echo ${GIT_PATH}
