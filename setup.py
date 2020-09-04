e5ece41a06a9138e21834682c8d42dc9ada273c6
jade <jadempage@outlook.com>
2020-09-04 10:55:18 +0100

    Updated setup.py files to work with PyPi

--- setup.py
+++ setup.py
@@ -1,74 +1,64 @@
-from setuptools import setup, find_packages
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 with open('requirements.txt') as req_txt:
     required = [line for line in req_txt.read().splitlines() if line]
 
-try:
-    # noinspection PyUnresolvedReferences
-    import pypandoc
-    # noinspection PyUnresolvedReferences
-    from os import path
-    here = path.abspath(path.dirname(__file__))
-    long_description = pypandoc.convert(path.join(here, 'README.md'), 'rst'),
-except RuntimeError:
-    long_description = ""
-except ImportError:
-    long_description = ""
-
-setup(
-    name='scs_analysis',
-    version='0.1.3',
-    description='Information management and analysis tools for South Coast Science data consumers.',
-    author='South Coast Science',
-    author_email='contact@southcoastscience.com',
-    url='https://github.com/south-coast-science/scs_analysis',
+setuptools.setup(
+    name="SCS_ANALYSIS",
+    version="0.0.3",
+    author="South Coast Science",
+    author_email="contact@southcoastscience.com",
+    description="Information management and analysis utilities for South Coast Science data consumers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/south-coast-science/scs_analysis",
     package_dir={'': 'src'},
-    packages=find_packages('src'),
+    packages=setuptools.find_packages('src'),
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX",
     ],
-    scripts=[
-        'src/scs_analysis/aws_api_auth.py',
-        'src/scs_analysis/aws_mqtt_client.py',
-        'src/scs_analysis/aws_mqtt_control.py',
-        'src/scs_analysis/aws_topic_history.py',
-        'src/scs_analysis/aws_topic_publisher.py',
-        'src/scs_analysis/csv_reader.py',
-        'src/scs_analysis/csv_writer.py',
-        'src/scs_analysis/histo_chart.py',
-        'src/scs_analysis/localised_datetime.py',
-        'src/scs_analysis/multi_chart.py',
-        'src/scs_analysis/node.py',
-        'src/scs_analysis/osio_api_auth.py',
-        'src/scs_analysis/osio_mqtt_client.py',
-        'src/scs_analysis/osio_mqtt_control.py',
-        'src/scs_analysis/osio_topic_history.py',
-        'src/scs_analysis/osio_topic_publisher.py',
-        'src/scs_analysis/sample_average.py',
-        'src/scs_analysis/sample_conv.py',
-        'src/scs_analysis/sample_error.py',
-        'src/scs_analysis/sample_interval.py',
-        'src/scs_analysis/sample_max.py',
-        'src/scs_analysis/sample_midpoint.py',
-        'src/scs_analysis/sample_min.py',
-        'src/scs_analysis/sample_regression.py',
-        'src/scs_analysis/single_chart.py',
-        'src/scs_analysis/socket_receiver.py',
-        'src/scs_analysis/uds_receiver.py',
-    ],
+    entry_points={
+        'console_scripts': [
+            'aws_api_auth = scs_analysis.aws_api_auth:main',
+            'aws_mqtt_client = scs_analysis.aws_mqtt_client:main',
+            'aws_mqtt_control = scs_analysis.aws_mqtt_control:main',
+            'aws_topic_history = scs_analysis.aws_topic_history:main',
+            'aws_topic_publisher = scs_analysis.aws_topic_publisher:main',
+            'csv_reader = scs_analysis.csv_reader:main',
+            'csv_writer = scs_analysis.csv_writer:main',
+            'histo_chart = scs_analysis.histo_chart:main',
+            'localised_datetime = scs_analysis.localised_datetime:main',
+            'multi_chart = scs_analysis.multi_chart:main',
+            'node = scs_analysis.node:main',
+            'osio_api_auth = scs_analysis.osio_api_auth:main',
+            'osio_mqtt_client = scs_analysis.osio_mqtt_client:main',
+            'osio_mqtt_control = scs_analysis.osio_mqtt_control:main',
+            'osio_topic_history = scs_analysis.osio_topic_history:main',
+            'osio_topic_publisher = scs_analysis.osio_topic_publisher:main',
+            'sample_average = scs_analysis.sample_average:main',
+            'sample_conv = scs_analysis.sample_conv:main',
+            'sample_error = scs_analysis.sample_error:main',
+            'sample_interval = scs_analysis.sample_interval:main',
+            'sample_max = scs_analysis.sample_max:main',
+            'sample_midpoint = scs_analysis.sample_midpoint:main',
+            'sample_min = scs_analysis.sample_min:main',
+            'sample_regression = scs_analysis.sample_regression:main',
+            'single_chart = scs_analysis.single_chart:main',
+            'socket_receiver = scs_analysis.socket_receiver:main',
+            'uds_receiver = scs_analysis.uds_receiver:main',
+        ],
+    },
     install_requires=required,
     platforms=['any'],
-    python_requires=">=3.3",
+    python_requires='>3.5',
     extras_require={
         'dev': [
             'pypandoc'
