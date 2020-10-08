95871ceaffd45a91fa9aceeccff73af7ddf6cc70
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-10-08 18:10:36 +0100

    Refactored Byline class

--- tests/aws/mananger/byline_manager_test.py
+++ tests/aws/mananger/byline_manager_test.py
@@ -6,8 +6,15 @@ Created on 7 Oct 2020
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 """
 
+import json
+
+from collections import OrderedDict
+
 from scs_core.aws.client.api_auth import APIAuth
+from scs_core.aws.data.byline import DeviceBylineGroup
 from scs_core.aws.manager.byline_manager import BylineManager
+
+from scs_core.data.json import JSONify
 
 from scs_host.sys.host import Host
 
@@ -45,11 +52,11 @@ for device in group.devices:
 print("=")
 
 # DeviceBylineGroup...
-group = manager.find_bylines_for_device('scs-bgx-431', excluded='/control')
+group = manager.find_bylines_for_device('scs-bgx-431')
 print(group)
 print("-")
 
-group = manager.find_bylines_for_device('scs-bgx-431')
+group = manager.find_bylines_for_device('scs-bgx-431', excluded='/control')
 print(group)
 print("-")
 
@@ -58,4 +65,15 @@ print("latest rec: %s" % group.latest_rec())
 print("-")
 
 print(group.device)
+print("=")
+
+jstr = JSONify.dumps(group.as_json())
+print(jstr)
 print("-")
+
+group = DeviceBylineGroup.construct_from_jdict(json.loads(jstr))
+print(group)
+print("-")
+
+print("latest pub: %s" % group.latest_pub())
+print("latest rec: %s" % group.latest_rec())
