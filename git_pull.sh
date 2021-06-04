5c0d31cb543accdb1e992bb85c3c1e34bb2ed8ca
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-06-04 08:53:23 +0100

    Updated git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -2,17 +2,7 @@
 
 # copy this file to the ~/SCS directory and edit as required...
 
-GIT_PATH=~/SCS/scs_airnow/                  # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
 GIT_PATH=~/SCS/scs_analysis/
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_comms_ge910/             # comment out if the package is not installed
 echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
@@ -22,47 +12,7 @@ echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
 
-GIT_PATH=~/SCS/scs_dev/                     # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_dfe_eng/                 # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_display_inky/            # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
 GIT_PATH=~/SCS/scs_host_posix/              #  replace with the appropriate host package, as necessary
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_mfr/                     # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_ndir/                    # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_osio/                    # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_philips_hue/             # comment out if the package is not installed
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
-GIT_PATH=~/SCS/scs_psu/                     # comment out if the package is not installed
 echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
