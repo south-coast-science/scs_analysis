8a1b4827c46e6d48a64909a09590c1d2f5c100a3
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-09-14 08:34:59 +0100

    Upgraded configuration_csv.py

--- src/scs_analysis/configuration_csv.py
+++ src/scs_analysis/configuration_csv.py
@@ -26,6 +26,15 @@ display-conf, gas-baseline, gas-model-conf, gps-conf, interface-conf, greengrass
 ndir-conf, opc-conf, pmx-model-conf, pressure-conf, psu-conf, psu-version, pt1000-calib, scd30-baseline, scd30-conf,
 schedule, shared-secret, sht-conf, sht-conf, networks, modem, sim, system-id, timezone-conf
 
+For separate and latest modes, two rec values are included:
+
+* rec.report - the most recent time that the device reported its status
+
+* rec.update - the most recent time that at least one of the fields changed value (not that those fields might not
+be included in the CSV file)
+
+For diff-histories and full-histories modes, a single rec value is included, equivalent to rec.update.
+
 Output CSV cell values are always wrapped in quotes ('"').
 
 SYNOPSIS
@@ -51,12 +60,15 @@ from subprocess import Popen, PIPE
 from scs_analysis.cmd.cmd_configuration_csv import CmdConfigurationCSV
 
 from scs_core.aws.client.monitor_auth import MonitorAuth
+from scs_core.aws.manager.configuration_check_finder import ConfigurationCheckFinder, ConfigurationCheckRequest
 from scs_core.aws.manager.configuration_finder import ConfigurationFinder, ConfigurationRequest
 
 from scs_core.data.datetime import LocalizedDatetime
 from scs_core.data.json import JSONify
 
 from scs_core.estate.configuration import Configuration
+
+from scs_core.sample.configuration_sample import ConfigurationReport
 
 from scs_core.sys.filesystem import Filesystem
 from scs_core.sys.http_exception import HTTPException
@@ -69,7 +81,7 @@ from scs_host.sys.host import Host
 
 def generate_csv(selected_configs, selected_nodes, file_path):
     jstr = '\n'.join([JSONify.dumps(config) for config in selected_configs])
-    node_args = ['tag', 'rec'] + ['val.' + selected_node for selected_node in selected_nodes]
+    node_args = ['tag', 'rec.report', 'rec.update'] + ['val.' + selected_node for selected_node in selected_nodes]
 
     if selected_nodes:
         p = Popen(['node.py', node_opts] + node_args, stdin=PIPE, stdout=PIPE)
@@ -130,21 +142,27 @@ if __name__ == '__main__':
             logger.error('incorrect password.')
             exit(1)
 
-        finder = ConfigurationFinder(requests, auth)
+        configuration_finder = ConfigurationFinder(requests, auth)
+        check_finder = ConfigurationCheckFinder(requests, auth)
 
         csv_opts = '-vsq' if cmd.verbose else '-sq'
         node_opts = '-v' if cmd.verbose else ''
 
+
         # ------------------------------------------------------------------------------------------------------------
         # run...
 
+        checks = check_finder.find(None, None, None, ConfigurationCheckRequest.MODE.FULL)
+        check_reports = {check.tag: check.rec for check in checks.items}
+
         if cmd.separate or cmd.latest:
-            response = finder.find(None, False, cmd.request_mode())
-            configs = sorted(response.items)
+            response = configuration_finder.find(None, False, cmd.request_mode())
+            configs = [ConfigurationReport.construct(item, check_reports[item.tag]) for item in sorted(response.items)]
 
         if cmd.separate:
             for node in node_names:
                 path = node + '.csv'
+                logger.info("-")
                 logger.info(path)
 
                 generate_csv(configs, [node], path)
@@ -155,20 +173,21 @@ if __name__ == '__main__':
 
         if cmd.histories:
             Filesystem.mkdir(cmd.histories)
-            tag_response = finder.find(None, False, ConfigurationRequest.MODE.TAGS_ONLY)
+            tag_response = configuration_finder.find(None, False, ConfigurationRequest.MODE.TAGS_ONLY)
 
             for tag in sorted(tag_response.items):
-                response = finder.find(tag, True, cmd.request_mode())
+                response = configuration_finder.find(tag, True, cmd.request_mode())
                 configs = sorted(response.items)
 
-                if len(configs) < 2:                # ignore single-row histories
-                    continue
-
                 path = os.path.join(cmd.histories, tag + '-configs.csv')
+                logger.info("-")
                 logger.info(path)
 
                 generate_csv(configs, cmd.nodes, path)
 
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # end...
 
     except KeyboardInterrupt:
         print(file=sys.stderr)
