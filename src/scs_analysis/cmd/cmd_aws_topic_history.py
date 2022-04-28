eb3e5a27cf7194e17eef5e15330f0802865ee950
Bruno Beloff <bruno.beloff@southcoastscience.com>
2022-04-28 12:08:53 +0100

    Building vcal_baseline_analysis.py CLU

--- src/scs_analysis/cmd/cmd_aws_topic_history.py
+++ src/scs_analysis/cmd/cmd_aws_topic_history.py
@@ -8,6 +8,7 @@ source repo: scs_analysis
 
 import logging
 import optparse
+import sys
 
 from scs_core.data.datetime import LocalizedDatetime
 from scs_core.data.timedelta import Timedelta
@@ -148,6 +149,8 @@ class CmdAWSTopicHistory(object):
 
 
     def is_valid_end(self):
+        print("self.__opts.end: %s" % self.__opts.end, file=sys.stderr)
+        print("self.end: %s" % self.end, file=sys.stderr)
         if self.__opts.end is None:
             return True
 
