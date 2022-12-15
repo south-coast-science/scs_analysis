6a0c39b6ec035a28264b6a3b0edd164578cccfca
Bruno Beloff <bruno.beloff@southcoastscience.com>
2022-12-15 15:43:44 +0000

    Added topic_roots() getter to TopicBylineGroup

--- src/scs_analysis/aws_byline.py
+++ src/scs_analysis/aws_byline.py
@@ -126,11 +126,9 @@ if __name__ == '__main__':
 
     except (ConnectionError, HTTPException) as ex:
         logger.error(repr(ex))
-        exit(1)
 
     except ResourceUnavailableException as ex:
         logger.error(repr(ex))
-        exit(1)
 
     except KeyboardInterrupt:
         print(file=sys.stderr)
