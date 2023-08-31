9ca0d528c7d5cc73d8a0e3ec7d75780cd5f7c2ae
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-08-31 13:15:22 +0100

    Upgraded CognitoUserIdentity JSON format

--- src/scs_analysis/cognito_users.py
+++ src/scs_analysis/cognito_users.py
@@ -24,7 +24,7 @@ cognito_users.py -Fe bruno.beloff@southcoastscience.com
 DOCUMENT EXAMPLE
 {"username": "506cd055-1978-4984-9f17-2fad77797fa1", "email": "bruno.beloff@southcoastscience.com",
 "given-name": "Bruno", "family-name": "Beloff", "confirmation-status": "CONFIRMED", "enabled": true,
-"email-verified": true, "is-super": false, "is-tester": false, "is-financial": false,
+"email-verified": true, "is-super": true, "is-tester": true, "is-financial": true,
 "created": "2023-04-20T11:45:21Z", "last-updated": "2023-06-26T14:39:17Z"}
 
 SEE ALSO
