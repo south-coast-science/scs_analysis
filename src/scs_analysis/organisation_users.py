75ce9f77081df5d2ca5a59c8f4e1451284a6dd10
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-09-25 14:33:26 +0100

    Added TODO

--- src/scs_analysis/organisation_users.py
+++ src/scs_analysis/organisation_users.py
@@ -50,6 +50,7 @@ from scs_core.sys.logging import Logging
 from scs_host.sys.host import Host
 
 
+# TODO: add Cognito users join
 # --------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
