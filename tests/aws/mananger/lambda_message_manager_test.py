37dc9770bdcd89590f431b53f7817ca4fc3ee6d1
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-10-06 10:39:45 +0100

    Cleaning

--- tests/aws/mananger/lambda_message_manager_test.py
+++ tests/aws/mananger/lambda_message_manager_test.py
@@ -6,6 +6,8 @@ Created on 23 May 2020
 @author: Bruno Beloff (bruno.beloff@southcoastscience.com)
 """
 
+import logging
+
 from scs_analysis.handler.batch_download_reporter import BatchDownloadReporter
 
 from scs_core.aws.manager.lambda_message_manager import MessageManager
@@ -17,19 +19,30 @@ from scs_core.sys.logging import Logging
 
 
 # --------------------------------------------------------------------------------------------------------------------
+Logging.config('test', level=logging.DEBUG)
+logger = Logging.getLogger()
 
-topic = "south-coast-science-dev/mobile/device/praxis-handheld-000008/status"
-# up_to = LocalizedDatetime.construct_from_iso8601('2020-05-24T00:00:00Z')
-up_to = LocalizedDatetime.now()
+topic = "south-coast-science-dev/cube/device/praxis-opcube-000001/status"
+# topic = "south-coast-science-dev/mobile/device/praxis-handheld-000008/status"
+
+start = LocalizedDatetime.construct_from_iso8601("2023-10-01T00:00:00Z")
+end = LocalizedDatetime.construct_from_iso8601("2023-10-01T00:01:00Z")
 
 # reporter...
 Logging.config('lambda_message_manager_test')
 reporter = BatchDownloadReporter()
 
 # message manager...
-message_manager = MessageManager(reporter=reporter)
+message_manager = MessageManager(reporter=reporter, api_key='NE33-2RL-1')
 print(message_manager)
 
-document = message_manager.find_latest_for_topic(topic, up_to, None, True, False, None)
-print(JSONify.dumps(document))
+#     def find_for_topic(self, topic, start, end, path, fetch_last, checkpoint, include_wrapper, rec_only,
+#                        min_max, exclude_remainder, fetch_last_written_before, backoff_limit): '**:/15:00'
+
+document = list(message_manager.find_for_topic(topic, start, end, None, False, None,
+                                               False, False, False, False, False, None))
+# print(JSONify.dumps(document))
+print("-")
+
+print(LocalizedDatetime.now().as_iso8601())
 
